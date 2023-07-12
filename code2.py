c7eda61d4ae0674984729da1aa260e88de3ebd14
sanghavi <sanghavi.a.r@diggibyte.com>
2023-07-12 17:23:58 +0530

    commit

--- code2.py
+++ code2.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 print("This is a feature branch")
 print("Commit one")
 print("Commit two")
 print("abc")
-=======
 print("This is a feature branch")
->>>>>>> 9af9c90d8029cd4e595c88a8d6491f78874ebdf8
+
