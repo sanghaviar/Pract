b9e43fde450073b415dea6aef699340941bfba0c
sanghavi <sanghavi.a.r@diggibyte.com>
2023-07-12 17:36:23 +0530

    cheerrycomit

--- code2.py
+++ code2.py
@@ -1,7 +1,13 @@
 print("This is a feature branch")
 print("Commit one")
 print("Commit two")
+<<<<<<< HEAD
 print("abc")
 print("This is a feature branch")
 print("cpick")
 
+=======
+print("changed")
+print("abcccc")
+print("adfsfdn")
+>>>>>>> b4c9442 (cpp)
